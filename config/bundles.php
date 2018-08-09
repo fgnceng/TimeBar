d8cd24f62d1a6192917d645cd73dcdc52a13a1f3
fgnceng <figen_sunal@outlook.com>
2018-08-09 10:48:28 +0300

    ip adress recieved

--- config/bundles.php
+++ config/bundles.php
@@ -19,5 +19,9 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+<<<<<<< Updated upstream
     MsgPhp\UserBundle\MsgPhpUserBundle::class => ['all' => true],
+=======
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+>>>>>>> Stashed changes
 ];
